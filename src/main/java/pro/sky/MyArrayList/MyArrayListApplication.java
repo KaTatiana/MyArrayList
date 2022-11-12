c342b40766d85a98260cab12a02ce210f403500a
KaTatiana <badinat@mail.ru>
2022-11-13 02:58:07 +0300

    Initial commit

---
+++ src/main/java/pro/sky/MyArrayList/MyArrayListApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.MyArrayList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyArrayListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyArrayListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/MyArrayList/MyArrayListApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.MyArrayList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyArrayListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
